2f677773e806ee6981bfa6e76defbd069f021e76
jarivatoi <jarivatoi@gmail.com>
2026-06-13 07:24:35 +0400

    ui: reduce search bar width to accommodate market dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1281,7 +1281,7 @@ function App() {
             {/* Search Input Container - Contains search + market dropdown */}
             <div className="flex-1 flex items-center gap-2">
               {/* Search Input */}
-              <div className="relative flex-1">
+              <div className="relative flex-1 min-w-0 max-w-[calc(100%-220px)]">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
                 <input
                   type="text"
