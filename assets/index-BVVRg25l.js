43be94ffad707c3e0ecb3a1e7e7b01301075705e
jarivatoi <jarivatoi@users.noreply.github.com>
2026-06-17 13:02:35 +0000

    deploy: ca96d1fab2875fc96ec8c821cf7db67f1fe002dd

--- assets/index-BVVRg25l.js
+++ assets/index-BVVRg25l.js
@@ -281,7 +281,7 @@ ${y}`}class ze extends Error{constructor({message:e,code:r,cause:n,name:s}){var 
         
         @keyframes roll {
           0% {
-            transform: translateX(calc(-100% - 48px)) translateY(-50%) rotate(0deg);
+            transform: translateX(calc(-100vw - 48px)) translateY(-50%) rotate(0deg);
             opacity: 0;
           }
           10% {
@@ -291,7 +291,7 @@ ${y}`}class ze extends Error{constructor({message:e,code:r,cause:n,name:s}){var 
             opacity: 1;
           }
           100% {
-            transform: translateX(calc(100% + 48px)) translateY(-50%) rotate(720deg);
+            transform: translateX(calc(100vw + 48px)) translateY(-50%) rotate(720deg);
             opacity: 0;
           }
         }
