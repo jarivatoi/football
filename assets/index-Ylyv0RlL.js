65cc6cdf69ed9a742807982496ead07f460458f3
jarivatoi <jarivatoi@users.noreply.github.com>
2026-06-17 15:32:04 +0000

    deploy: 7460bf8a50523374daeb63dbaf7d65b47492c00e

--- assets/index-Ylyv0RlL.js
+++ assets/index-Ylyv0RlL.js
@@ -291,7 +291,7 @@ ${y}`}class ze extends Error{constructor({message:e,code:r,cause:n,name:s}){var 
             opacity: 1;
           }
           100% {
-            transform: translateX(calc(100% + 48px)) translateY(-50%) rotate(720deg);
+            transform: translateX(calc(100% + 48px)) translateY(-50%) rotate(0deg);
             opacity: 0;
           }
         }
