ca96d1fab2875fc96ec8c821cf7db67f1fe002dd
jarivatoi <jarivatoi@gmail.com>
2026-06-17 17:02:02 +0400

    fix: ball animation uses full screen width
    - Changed from 100% (container width) to 100vw (viewport width)
    - Ball now travels across entire screen
    - Start: calc(-100vw - 48px) - off-screen left
    - End: calc(100vw + 48px) - off-screen right
    - Text stays centered in 420px container
    - Ball spans full device screen width

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -698,7 +698,7 @@ const UserLogin: React.FC<UserLoginProps> = ({ onLoginSuccess }) => {
         
         @keyframes roll {
           0% {
-            transform: translateX(calc(-100% - 48px)) translateY(-50%) rotate(0deg);
+            transform: translateX(calc(-100vw - 48px)) translateY(-50%) rotate(0deg);
             opacity: 0;
           }
           10% {
@@ -708,7 +708,7 @@ const UserLogin: React.FC<UserLoginProps> = ({ onLoginSuccess }) => {
             opacity: 1;
           }
           100% {
-            transform: translateX(calc(100% + 48px)) translateY(-50%) rotate(720deg);
+            transform: translateX(calc(100vw + 48px)) translateY(-50%) rotate(720deg);
             opacity: 0;
           }
         }
